df23e272097da23f6b148184406cc0b121b7f6c0
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-10-17 16:40:25 -0400

    test for the video

--- src/App.spec.js
+++ src/App.spec.js
@@ -5,9 +5,13 @@ import App from './App'
 describe('App', () => {
   it('works', () => {
     mount(<App />)
-    cy.get('[type=submit]').should('be.disabled')
-    cy.contains('label', 'Name').find('input').type('Gleb')
+    // submit should be disabled
+    cy.get('input[type=submit]').should('be.disabled')
+    // type name, and the
+    cy.contains('label', 'Name:').find('input').type('Gleb')
+    // welcome message should have that name
     cy.contains('h1', 'Welcome Gleb')
-    cy.contains('label', 'Age').find('input[type=number]')
+    // Age input should have type number
+    cy.contains('label', 'Age:').find('input[type=number]')
   })
 })
